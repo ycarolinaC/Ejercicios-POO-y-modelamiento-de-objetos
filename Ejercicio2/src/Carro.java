8365b0cd07c3d26c8d4a33c66e7a554ae85e11e4
Carolina <y.carolmosquera@gmail.com>
2022-04-17 22:30:35 -0500

    Programa que tiene el método main en las dos clases

---
+++ Ejercicio2/src/Carro.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Carolina
+ */
+public class Carro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String color;
+       
+       
+    }
+
+    public Carro(String color) {
+        System.out.println("El carro es color "+color);
+    }
+    
+}
